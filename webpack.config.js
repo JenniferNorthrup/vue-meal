9fec0c21a253ca29e20ea4165b619567be6a0026
Jennifer Northrup <jenniferhnorthrup@gmail.com>
2020-10-04 18:44:27 -0400

    Change build directory from dist to app

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,11 @@ module.exports = {
   entry: './src/index.js',
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      'vue$': 'vue/app/vue.esm.js'
     }
   },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'app'),
   },
 };
